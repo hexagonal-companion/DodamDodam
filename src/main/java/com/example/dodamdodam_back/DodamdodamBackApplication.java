65f28f02e1dab8126d6bd82b45a248fa73df9203
Bak Hyegyeong <calla3494qhk@naver.com>
2024-09-20 14:32:48 +0900

    first commit

---
+++ src/main/java/com/example/dodamdodam_back/DodamdodamBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dodamdodam_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodamdodamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodamdodamBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dodamdodam_back/DodamdodamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dodamdodam_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
